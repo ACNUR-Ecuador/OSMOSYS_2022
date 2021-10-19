23c5f874186b8d9c795e98c1d3d541ce3329b1da
sebasstg <sebassalazart@gmail.com>
2021-10-18 21:07:03 -0500

    pantalla administracion organizaciones

---
+++ frontend/src/app/administration/organization-administration/organization-administration.component.ts
@@ -0,0 +1,162 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Organization} from '../../shared/model/OsmosysModel';
+import {ColumnDataType, ColumnTable, EnumsType} from '../../shared/model/UtilsModel';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {UtilsService} from '../../shared/services/utils.service';
+import {EnumsService} from '../../shared/services/enums.service';
+import {OrganizationService} from '../../shared/services/organization.service';
+
+@Component({
+  selector: 'app-organization-administration',
+  templateUrl: './organization-administration.component.html',
+  styleUrls: ['./organization-administration.component.scss']
+})
+export class OrganizationAdministrationComponent implements OnInit {
+    items: Organization[];
+    cols: ColumnTable[];
+    showDialog = false;
+    private submitted = false;
+    formItem: FormGroup;
+    private states: string[];
+    // tslint:disable-next-line:variable-name
+    _selectedColumns: ColumnTable[];
+
+    constructor(
+        private messageService: MessageService,
+        private confirmationService: ConfirmationService,
+        private fb: FormBuilder,
+        public utilsService: UtilsService,
+        private enumsService: EnumsService,
+        private organizationService: OrganizationService
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.loadItems();
+        this.cols = [
+            {field: 'id', header: 'Id', type: ColumnDataType.numeric},
+            {field: 'code', header: 'Código', type: ColumnDataType.text},
+            {field: 'acronym', header: 'Acrónimo', type: ColumnDataType.text},
+            {field: 'description', header: 'Descripción', type: ColumnDataType.text},
+            {field: 'state', header: 'Estado', type: ColumnDataType.text}
+        ];
+        this._selectedColumns = this.cols.filter(value => value.field !== 'id');
+
+        this.formItem = this.fb.group({
+            id: new FormControl(''),
+            code: new FormControl('', Validators.required),
+            state: new FormControl('', Validators.required),
+            description: new FormControl('', Validators.required),
+            acronym: new FormControl('', Validators.required),
+        });
+
+        this.enumsService.getByType(EnumsType.State).subscribe(value => {
+            this.states = value;
+        });
+
+    }
+
+    private loadItems() {
+        this.organizationService.getAll().subscribe(value => {
+            this.items = value;
+        }, error => {
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Error al cargar los organizationos',
+                detail: error.error.message,
+                life: 3000
+            });
+        });
+    }
+
+    exportExcel() {
+        import('xlsx').then(xlsx => {
+            const itemsRenamed = this.utilsService.renameKeys(this.items, this.cols);
+            const worksheet = xlsx.utils.json_to_sheet(itemsRenamed);
+            const workbook = {Sheets: {data: worksheet}, SheetNames: ['data']};
+            const excelBuffer: any = xlsx.write(workbook, {bookType: 'xlsx', type: 'array'});
+            this.utilsService.saveAsExcelFile(excelBuffer, 'organizationos');
+        });
+    }
+
+    createItem() {
+        this.messageService.clear();
+        this.utilsService.resetForm(this.formItem);
+        this.submitted = false;
+        this.showDialog = true;
+        const newItem = new Organization();
+        this.formItem.patchValue(newItem);
+        console.log(this.formItem.value);
+    }
+
+    editItem(organization: Organization) {
+        this.utilsService.resetForm(this.formItem);
+        this.submitted = false;
+        this.showDialog = true;
+        this.formItem.patchValue(organization);
+    }
+
+
+    saveItem() {
+        this.messageService.clear();
+        const {
+            id,
+            code,
+            state,
+            description,
+            acronym
+        }
+            = this.formItem.value;
+        const organization: Organization = {
+            id,
+            code,
+            state,
+            description,
+            acronym
+        };
+        if (organization.id) {
+            // tslint:disable-next-line:no-shadowed-variable
+            this.organizationService.update(organization).subscribe(id => {
+                this.cancelDialog();
+                this.loadItems();
+            }, error => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error al actualizar la organización',
+                    detail: error.error.message,
+                    life: 3000
+                });
+            });
+        } else {
+            // tslint:disable-next-line:no-shadowed-variable
+            this.organizationService.save(organization).subscribe(id => {
+                this.cancelDialog();
+                this.loadItems();
+            }, error => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error al guardar la organización',
+                    detail: error.error.message,
+                    life: 3000
+                });
+            });
+        }
+
+    }
+
+    cancelDialog() {
+        this.showDialog = false;
+        this.submitted = false;
+    }
+
+    @Input() get selectedColumns(): any[] {
+        return this._selectedColumns;
+    }
+
+    set selectedColumns(val: any[]) {
+        // restore original order
+        this._selectedColumns = this.cols.filter(col => val.includes(col));
+    }
+
+}
---
+++ frontend/src/app/shared/services/organization.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Organization} from '../model/OsmosysModel';
+import {environment} from '../../../environments/environment';
+
+const mainServiceUrl = environment.base_url + '/organizations';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    public getAll(): Observable<Organization[]> {
+        return this.http.get<Organization[]>(`${mainServiceUrl}`);
+    }
+
+    public save(organization: Organization): Observable<number> {
+        return this.http.post<number>(`${mainServiceUrl}`, organization);
+    }
+
+    public update(organization: Organization): Observable<number> {
+        return this.http.put<number>(`${mainServiceUrl}`, organization);
+    }
+}
