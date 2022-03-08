3fe468834344ce11e486e8ea56f57c7a94facfb9
sebasstg <sebassalazart@gmail.com>
2022-03-08 14:36:17 -0500

    accesos a catálogo de indicadores

--- backend/src/main/java/org/unhcr/osmosys/reports/service/ReportDataService.java
+++ backend/src/main/java/org/unhcr/osmosys/reports/service/ReportDataService.java
@@ -1,18 +1,17 @@
 package org.unhcr.osmosys.reports.service;
 
 import com.sagatechs.generics.exceptions.GeneralAppException;
-import com.sagatechs.generics.persistence.model.State;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.unhcr.osmosys.services.IndicatorExecutionService;
 import org.unhcr.osmosys.webServices.model.IndicatorExecutionWeb;
 import org.unhcr.osmosys.webServices.model.LateType;
-import org.unhcr.osmosys.webServices.model.MonthWeb;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
 @Stateless
@@ -41,29 +40,8 @@ public class ReportDataService {
             map.put("totalExecution", ie.getTotalExecution());
             map.put("executionPercentage", ie.getExecutionPercentage());
             map.put("late", ie.getLate().equals(LateType.LATE) ? "Si" : "No");
-            // late months
-           /* if (ie.getLate() && CollectionUtils.isNotEmpty(ie.getLateMonths())) {
-                List<String> months = ie.getLateMonths()
-                        .stream()
-                        .filter(monthWeb -> monthWeb.getState().equals(State.ACTIVO))
-                        .sorted(Comparator.comparingInt(MonthWeb::getOrder))
-                        .map(monthWeb -> monthWeb.getMonth().getLabel() + "-" + monthWeb.getYear())
-                        .collect(Collectors.toList());
-                if (CollectionUtils.isNotEmpty(months)) {
-                    map.put("lateMonths", String.join("; ", months));
-                }
-            }*/
             r.add(map);
         }
-
-        // ordeno
-        /*r = r.stream()
-                .sorted(Comparator.comparing(o -> ((String) o.get("indicator"))))
-                //.sorted(Comparator.comparing(o -> ((String) o.get("outcomeStatement"))))
-                .sorted(Comparator.comparing(o -> ((String) o.get("indicatorType"))))
-                .sorted(Comparator.comparing(o -> ((String) o.get("project"))))
-                .sorted(Comparator.comparing(o -> ((String) o.get("partner"))))
-                .collect(Collectors.toList());*/
 
         return r;
 
