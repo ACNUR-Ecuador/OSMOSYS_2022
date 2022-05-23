653283c2d0e9d99381da182368d4b0dee8164091
sebasstg <sebassalazart@gmail.com>
2022-05-23 18:21:14 -0500

    correccion reportes exportacion

---
+++ frontend/src/app/reports/powerbi/rba-indicators/rba-indicators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rba-indicators',
+  templateUrl: './rba-indicators.component.html',
+  styleUrls: ['./rba-indicators.component.scss']
+})
+export class RbaIndicatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
