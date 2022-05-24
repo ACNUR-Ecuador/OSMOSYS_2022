d092a4bf02c7ba801296d9d1f8d7400417462cde
sebasstg <sebassalazart@gmail.com>
2022-05-24 16:02:24 -0500

    Indicadores AI

---
+++ frontend/src/app/reports/powerbi/activity-info-indicators/activity-info-indicators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-info-indicators',
+  templateUrl: './activity-info-indicators.component.html',
+  styleUrls: ['./activity-info-indicators.component.scss']
+})
+export class ActivityInfoIndicatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
