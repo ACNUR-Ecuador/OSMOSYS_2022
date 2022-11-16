edb874416f7109d286c83322f082c6ed18b4b23b
sebasstg <sebassalazart@gmail.com>
2022-11-16 15:54:10 -0500

    borro fronted 2022

--- frontend/src/environments/environment.hmr.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    hmr: true,
-    locale: 'es',
-    base_url: 'http://localhost:8080/osmosys/api',
-    app_code: 'OSMOSYS',
-    versionCheckURL: 'https://imecuador.unhcr.org/programs/version.json',
-};
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    hmr: false,
-    locale: 'es',
-    base_url: 'https://backend.imecuador.unhcr.org/osmosys/api',
-    app_code: 'OSMOSYS',
-    versionCheckURL: 'https://imecuador.unhcr.org/osmosys/version.json',
-};
