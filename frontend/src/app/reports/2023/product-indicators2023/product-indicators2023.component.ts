dc7c9addbdd61ec56ea2fb18aa19c61e8b82b9e5
sebasstg <sebassalazart@gmail.com>
2023-03-13 18:09:23 -0500

    powerbi

---
+++ frontend/src/app/reports/2023/product-indicators2023/product-indicators2023.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-indicators2023',
+  templateUrl: './product-indicators2023.component.html',
+  styleUrls: ['./product-indicators2023.component.scss']
+})
+export class ProductIndicators2023Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
