0a0cc883e2f96df4823c2ba2ce7e4948343d6cfb
sebasstg <sebassalazart@gmail.com>
2023-03-15 16:53:19 -0500

    tablero pi

---
+++ frontend/src/app/reports/2023/pi-indicators2023/pi-indicators2023.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pi-indicators2023',
+  templateUrl: './pi-indicators2023.component.html',
+  styleUrls: ['./pi-indicators2023.component.scss']
+})
+export class PiIndicators2023Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
