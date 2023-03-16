504f47a3a9f63b9aeeb2d7da99aa54648b8f783f
maferayala <maferayala_92@hotmail.com>
2023-03-16 15:24:24 -0500

    Nuevo tablero de tendencia de indicadores 2023 en la pestaña reports

---
+++ frontend/src/app/reports/2023/trends-indicators2023/trends-indicators2023.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trends-indicators2023',
+  templateUrl: './trends-indicators2023.component.html',
+  styleUrls: ['./trends-indicators2023.component.scss']
+})
+export class TrendsIndicators2023Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
