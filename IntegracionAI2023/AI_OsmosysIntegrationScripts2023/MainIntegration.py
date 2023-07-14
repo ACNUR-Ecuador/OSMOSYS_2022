3c175df88bd0ed8f93ea5693fe1b45c1aeeff612
Fernanda Ayala <maferayala_92@hotmail.com>
2023-07-14 09:30:56 -0500

    Integration june, and front end change in reports menu for partners

--- IntegracionAI2023/AI_OsmosysIntegrationScripts2023/MainIntegration.py
+++ IntegracionAI2023/AI_OsmosysIntegrationScripts2023/MainIntegration.py
@@ -32,8 +32,8 @@ import importScripts.Subform_S3_01
 
 print('Integration')
 
-month = 'MAYO'
-month_number = '2023-05'
+month = 'JUNIO'
+month_number = '2023-06'
 year = 2023
 test = True
 print('month'+str(month))
--- frontend/src/app/layout/app.menu.service.ts
+++ frontend/src/app/layout/app.menu.service.ts
@@ -226,19 +226,19 @@ export class MenuService {
         {
             label: 'Reportes',
             icon: 'pi pi-file-o',
-            roles: ['SUPER_ADMINISTRADOR', 'ADMINISTRADOR', 'EJECUTOR_ID', 'MONITOR_ID', 'MONITOR_PROYECTOS', 'EJECUTOR_PROYECTOS', 'PUNTO_FOCAL'],
+            roles: ['SUPER_ADMINISTRADOR', 'ADMINISTRADOR', 'EJECUTOR_ID', 'MONITOR_ID','PUNTO_FOCAL'],
             items: [
                 {
                     label: 'Catálogo de Indicadores',
                     icon: 'pi pi-fw pi-file-excel',
                     routerLink: ['/reports/indicatorsCatalog'],
-                    roles: ['SUPER_ADMINISTRADOR', 'ADMINISTRADOR', 'EJECUTOR_ID', 'MONITOR_ID', 'MONITOR_PROYECTOS', 'EJECUTOR_PROYECTOS'],
+                    roles: ['SUPER_ADMINISTRADOR', 'ADMINISTRADOR', 'EJECUTOR_ID', 'MONITOR_ID'],
                 },
                 {
                     label: 'Exportación de Datos',
                     icon: 'pi pi-fw pi-file-excel',
                     routerLink: ['/reports/dataExport'],
-                    roles: ['SUPER_ADMINISTRADOR', 'ADMINISTRADOR', 'EJECUTOR_ID', 'MONITOR_ID', 'MONITOR_PROYECTOS', 'EJECUTOR_PROYECTOS'],
+                    roles: ['SUPER_ADMINISTRADOR', 'ADMINISTRADOR', 'EJECUTOR_ID', 'MONITOR_ID'],
                 },
                 {
                     label: 'Reportes de retrasos',
