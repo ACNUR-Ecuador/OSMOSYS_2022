6f77f8dd1be2b1c85a21c1d1edea664ede99e614
Fernanda Ayala <maferayala_92@hotmail.com>
2023-09-13 15:11:52 -0500

    Modified the filter of the productIndicators2023Osmosys.pbix Integration w/ AI August

--- IntegracionAI2023/AI_OsmosysIntegrationScripts2023/MainIntegration.py
+++ IntegracionAI2023/AI_OsmosysIntegrationScripts2023/MainIntegration.py
@@ -32,8 +32,8 @@ import importScripts.Subform_S3_01
 
 print('Integration')
 
-month = 'JULIO'
-month_number = '2023-07'
+month = 'AGOSTO'
+month_number = '2023-08'
 year = 2023
 test = True
 print('month'+str(month))
