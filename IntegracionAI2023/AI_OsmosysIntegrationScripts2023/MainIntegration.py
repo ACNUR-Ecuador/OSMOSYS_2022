8cbfe6a50f7f0adedb91bd9217340ed7db528e5f
Fernanda Ayala <maferayala_92@hotmail.com>
2023-10-16 13:03:14 -0500

    AI Osmosys Integration Septiembre

--- IntegracionAI2023/AI_OsmosysIntegrationScripts2023/MainIntegration.py
+++ IntegracionAI2023/AI_OsmosysIntegrationScripts2023/MainIntegration.py
@@ -32,8 +32,8 @@ import importScripts.Subform_S3_01
 
 print('Integration')
 
-month = 'AGOSTO'
-month_number = '2023-08'
+month = 'SEPTIEMBRE'
+month_number = '2023-09'
 year = 2023
 test = True
 print('month'+str(month))
