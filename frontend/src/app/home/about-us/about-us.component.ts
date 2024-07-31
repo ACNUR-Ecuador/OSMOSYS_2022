4fe9bd2bbe78f649c62b7a68e73b25918cd28679
sebasstg <sebassalazart@gmail.com>
2024-07-31 08:23:39 -0600

    Abourt US

---
+++ frontend/src/app/home/about-us/about-us.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-about-us',
+    templateUrl: './about-us.component.html',
+    styleUrls: ['./about-us.component.scss']
+})
+export class AboutUsComponent {
+
+    visibleMember: number = -1;
+
+
+    constructor() {
+    }
+
+
+}
