280fc81d5fdf7e3c2f090250a455526113c0c06d
danygancino <danygancino@gmail.com>
2025-05-09 18:21:16 -0500

    T2:  Fix with updating indicator execution disaggregation options for indicators with active  non-disaggregation and other dissagregations

--- backend/src/main/java/org/unhcr/osmosys/services/IndicatorExecutionService.java
+++ backend/src/main/java/org/unhcr/osmosys/services/IndicatorExecutionService.java
@@ -1355,7 +1355,7 @@ public class IndicatorExecutionService {
         Set<StandardDissagregationOption> dissOptionsToDissable = new HashSet<>();
         /*Actualizando por cambios en el Indicador*/
         for (DissagregationAssignationToIndicator dissagregationAssignationToIndicator : indicatorExecution.getIndicator().getDissagregationsAssignationToIndicator()) {
-            if(dissagregationAssignationToIndicator.getDissagregationType().equals(DissagregationType.SIN_DESAGREGACION) && Objects.equals(dissagregationAssignationToIndicator.getPeriod().getId(), indicatorExecution.getPeriod().getId()) && dissagregationAssignationToIndicator.getState().equals(State.ACTIVO)){
+            if(dissagregationAssignationToIndicator.getDissagregationType().equals(DissagregationType.SIN_DESAGREGACION) && Objects.equals(dissagregationAssignationToIndicator.getPeriod().getId(), indicatorExecution.getPeriod().getId()) && dissagregationAssignationToIndicator.getState().equals(State.ACTIVO) && indicatorExecution.getIndicator().getDissagregationsAssignationToIndicator().size()==1){
                 dissOptionsToDissable=indicatorExecution.getIndicatorExecutionDissagregationAssigments().stream().map(IndicatorExecutionDissagregationAssigment::getDisagregationOption).collect(Collectors.toSet());
                 break;
             }
@@ -1437,7 +1437,7 @@ public class IndicatorExecutionService {
                 .map(option -> option.getDisagregationOption().getId())
                 .collect(Collectors.toSet());
         for (DissagregationAssignationToIndicator dissagregationAssignationToIndicator : indicatorExecution.getIndicator().getDissagregationsAssignationToIndicator()) {
-            if (dissagregationAssignationToIndicator.getDissagregationType().equals(DissagregationType.SIN_DESAGREGACION) && Objects.equals(dissagregationAssignationToIndicator.getPeriod().getId(), indicatorExecution.getPeriod().getId()) && dissagregationAssignationToIndicator.getState().equals(State.ACTIVO)) {
+            if (dissagregationAssignationToIndicator.getDissagregationType().equals(DissagregationType.SIN_DESAGREGACION) && Objects.equals(dissagregationAssignationToIndicator.getPeriod().getId(), indicatorExecution.getPeriod().getId()) && dissagregationAssignationToIndicator.getState().equals(State.ACTIVO) && indicatorExecution.getIndicator().getDissagregationsAssignationToIndicator().size()==1) {
                 dissOptionsToDissable = indicatorExecution.getIndicatorExecutionDissagregationAssigments().stream().map(IndicatorExecutionDissagregationAssigment::getDisagregationOption).collect(Collectors.toSet());
                 break;
             }
